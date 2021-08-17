9fe10312c8b2daf666b2fa76c9295cba12efa000
TheRealDeathEye <therealdeatheye@gmail.com>
2021-08-17 06:43:44 -0500

    about

--- src/components/aboutMe.jsx
+++ src/components/aboutMe.jsx
@@ -8,9 +8,17 @@ function AboutMe() {
       <Card style={{width:'85%', maxWidth:'500px', margin:'25px 0 25px 0'}}>
         <Card.Header style={{fontSize: "1.6em", backgroundColor:'#212529', color:'white'}}  className=" text-center">About Me</Card.Header>
         <Card.Body style={{background:'#c7c4c3'}}>
-          <p>I am a Computer Engineering student attending University of Missouri - Columbia.</p>
+          <p>I am a Computer Engineering student attending University of Missouri - Columbia. CEO of a tech startup based in St Louis, Missouri.</p>
         </Card.Body>
       </Card>
+      <div className='row' style={{width:'65%', maxWidth:'300px'}}>
+        <div className='col'>
+          <a href='https://github.com/TheRealDeathEye'><img src='imgs/github.png' alt='' style={{width:'40px', height:'40px'}} /></a>
+        </div>
+        <div className='col'>
+          <a href='https://www.linkedin.com/in/trevor-orr/'><img src='imgs/linkedin.png' alt='' style={{width:'40px', height:'40px'}} /></a>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,6 +4,7 @@ function Home() {
   return(
     <div align='center'>
       <h1 style={{fontSize: "4em", marginTop:'100px'}}>Trevor Orr</h1>
+      <h2 style={{fontSize: "3em", marginTop:'100px'}}>Embarking on a Freelancing Journey</h2>
     </div>
   );
 }
