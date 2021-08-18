3af027b741df9dc593301ff4e8c8e392ff29eea7
TheRealDeathEye <therealdeatheye@gmail.com>
2021-08-18 15:48:57 -0500

    Update home.jsx

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,7 +4,7 @@ function Home() {
   return(
     <div align='center'>
       <h1 style={{fontSize: "4em", marginTop:'150px'}}>Trevor Orr</h1>
-      <h2 style={{fontSize: "3em", marginTop:'100px'}}>Embarking on a Freelancing Journey</h2>
+      <h2 style={{fontSize: "3em", marginTop:'100px'}}>Embarking on a Coding Journey</h2>
     </div>
   );
 }
